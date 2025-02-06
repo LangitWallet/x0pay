b1e571bef384c8af6372216f26a91fd2096faea2
apologeth <newblockchainworks@gmail.com>
2025-02-06 11:48:05 +0700

    default network

--- packages/kit-bg/src/services/ServiceAccount/defaultNetworkAccountsConfig.ts
+++ packages/kit-bg/src/services/ServiceAccount/defaultNetworkAccountsConfig.ts
@@ -33,7 +33,7 @@ export function buildDefaultAddAccountNetworks() {
     deriveType: IAccountDeriveTypesEvm;
   }[] = [
     {
-      networkId: networkIdsMap.eth,
+      networkId: networkIdsMap.xai,
       deriveType: 'default',
     },
     // {
