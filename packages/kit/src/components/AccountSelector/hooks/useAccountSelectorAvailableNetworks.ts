cac74f0262d8dee7e828d55c88a9c9bb22743977
apologeth <newblockchainworks@gmail.com>
2025-02-07 08:59:19 +0700

    default network

--- packages/kit/src/components/AccountSelector/hooks/useAccountSelectorAvailableNetworks.ts
+++ packages/kit/src/components/AccountSelector/hooks/useAccountSelectorAvailableNetworks.ts
@@ -32,6 +32,6 @@ export function useAccountSelectorAvailableNetworks({
 
   return {
     networkIds,
-    defaultNetworkId: '660279'//availableNetworksInfo?.defaultNetworkId,
+    defaultNetworkId: availableNetworksInfo?.defaultNetworkId,
   };
 }
